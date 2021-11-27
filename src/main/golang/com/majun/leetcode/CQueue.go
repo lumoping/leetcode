99012b15b1d726abe8116e3c9ec32872dadf91db
majun <864263375@qq.com>
2021-11-27 19:40:09 +0800

    golang实现

---
+++ src/main/golang/com/majun/leetcode/CQueue.go
@@ -0,0 +1,32 @@
+package leetcode
+
+import "container/list"
+
+type CQueue struct {
+	stack, reveredStack *list.List
+}
+
+func Constructor() CQueue {
+	return CQueue{
+		stack:        list.New(),
+		reveredStack: list.New(),
+	}
+}
+
+func (this *CQueue) AppendTail(value int) {
+	this.stack.PushBack(value)
+}
+
+func (this *CQueue) DeleteHead() int {
+	if this.reveredStack.Len() == 0 {
+		for this.stack.Len() > 0 {
+			this.reveredStack.PushBack(this.stack.Remove(this.stack.Back()))
+		}
+	}
+	if this.reveredStack.Len() != 0 {
+		head := this.reveredStack.Back()
+		this.reveredStack.Remove(head)
+		return head.Value.(int)
+	}
+	return -1
+}
