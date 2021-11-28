c165bdc40115f0e1a1676389154334b8f413c398
majun <864263375@qq.com>
2021-11-28 17:46:53 +0800

    测试gitcheck

--- src/main/java/com/majun/leetcode/CQueue.java
+++ src/main/java/com/majun/leetcode/CQueue.java
@@ -3,6 +3,7 @@ package com.majun.leetcode;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+//测试gitcheck
 /**
  * @author majun 2021-11-27
  * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead操作返回 -1 )
